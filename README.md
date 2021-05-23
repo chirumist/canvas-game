314aaee91a0aefc262057ca1b7f0d614cb6bdb50
Chirag Mistry <chiragmistry@goldenmace.com>
2021-05-23 20:05:20 +0530

    -remove projecttile after touch enemy and remove from canvas

--- README.md
+++ README.md
@@ -3,10 +3,10 @@ Demo Line: https://chirumist.github.io/canvas-game/
 45:40
 - Create Player (Done)
 - Shoot projectile (Done)
-- Create Enemies
-- Detect collision on enemy / projectile hit
-- Detect collision on enemy / player hit
-- Remove Offscreen Project tile
+- Create Enemies (Done)
+- Detect collision on enemy / projectile hit (Done)
+- Detect collision on enemy / player hit (Done)
+- Remove Offscreen Project tile (Done)
 - Colorize game
 - Shrink enemy on hit
 - Create partical explosion on hit
--- js/index.js
+++ js/index.js
@@ -14,7 +14,7 @@ const playerHeight = canvas.height / 2
 const player = new Player(c, playerWidth, playerHeight, 30, 'blue')
 
 const projectTiles = []
-
+// New Enemy
 const enemies = []
 let enemySpawnSpeed = 2000
 const spawnEnemy = () => {
@@ -39,20 +39,61 @@ const spawnEnemy = () => {
     }, enemySpawnSpeed);
 }
 
+// Random Function
 const random = () => {
     return Math.random()
 }
+
+// Game Loop
+let animateId
 const main = () => {
-    window.requestAnimationFrame(main)
+    animateId = window.requestAnimationFrame(main)
     c.clearRect(0,0,canvas.width, canvas.height)
+    // Spawn Player
     player.x = window.innerWidth / 2
     player.y = window.innerHeight / 2
     player.draw()
-    projectTiles.forEach((pTile) => {
+
+    // Spawn Projectile
+    projectTiles.forEach((pTile, pTileIndex) => {
         pTile.update()
+        // Remove project tile off screen
+        if (
+            pTile.x + pTile.radius < 0 ||
+            pTile.x - pTile.radius > canvas.width ||
+            pTile.y + pTile.radius < 0 ||
+            pTile.y - pTile.radius > canvas.height
+            ) {
+            setTimeout(() => {
+                projectTiles.splice(pTileIndex, 1)
+            }, 0);
+        }
     })
-    enemies.forEach((enemy) => {
+
+    // Spawn enemy
+    enemies.forEach((enemy, enmIndex) => {
         enemy.update()
+
+        // Distance between player and enemy
+        const enemyHits = Math.hypot(player.x - enemy.x, player.y - enemy.y)
+
+        // Game Over
+        if (enemyHits - enemy.radius - player.radius < 1) {
+            window.cancelAnimationFrame(animateId)
+        }
+        // Touch enemy
+        projectTiles.forEach((pTile, pTileIndex) => {
+            // Distance between enemy and projecttile
+            const projectHit = Math.hypot(pTile.x - enemy.x, pTile.y - enemy.y)
+
+            if (projectHit - enemy.radius - pTile.radius < 1) {
+                // Enemy hits
+                setTimeout(() => {
+                    enemies.splice(enmIndex, 1)
+                    projectTiles.splice(pTileIndex, 1)
+                }, 0);
+            }
+        })
     })
 }
 
@@ -60,11 +101,14 @@ const main = () => {
 window.addEventListener('click', (e) => {
     const width = canvas.width / 2
     const height = canvas.height / 2
+
+    // Create angle for projecttile distance
     const angle = Math.atan2(e.clientX - width,e.clientY - height)
     const velocity = {
         x: Math.sin(angle),
         y: Math.cos(angle)
     }
+    // New projecttile
     const projectile = new Projectile(c ,width, height, 5, 'red', velocity )
     projectTiles.push(projectile)
 })
